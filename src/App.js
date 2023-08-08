039d6bc76ee285f485fda7ac024706d7d14cf147
Vivechan B <52950173+vivechanchanny@users.noreply.github.com>
2023-08-08 13:39:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          vivechan poc for CICD 
+          vivechan poc for CICD try 1
         </p>
       </header>
     </div>
